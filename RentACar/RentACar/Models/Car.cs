86c8595635fb37808284592c46d0ca1e9f2a82be
Patricio <pato.rocaspana@gmail.com>
2021-08-31 01:49:23 -0300

    Create class Car Model

---
+++ RentACar/RentACar/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public CarsBrands Brand {get; set;}
+        public string Model { get; set; }
+        public int DoorsQuantity { get; set; }
+        public string Color { get; set; }
+        public Transmission Transmission { get; set; }
+    }
+}
---
+++ RentACar/RentACar/Models/Transmission.cs
@@ -0,0 +1,8 @@
+﻿namespace RentACar
+{
+    public enum Transmission
+    {
+        Manual = 0,
+        Automatic = 1
+    }
+}
