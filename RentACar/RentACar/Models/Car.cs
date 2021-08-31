f3a8ac284b6151fc2b678df76c629f5a78a1f608
Patricio <pato.rocaspana@gmail.com>
2021-08-31 13:47:44 -0300

    Add constructors to class Car

--- RentACar/RentACar/Models/Car.cs
+++ RentACar/RentACar/Models/Car.cs
@@ -9,10 +9,25 @@ namespace RentACar
     public class Car
     {
         public int Id { get; set; }
-        public CarsBrands Brand {get; set;}
+        public CarsBrands Brand { get; set; }
         public string Model { get; set; }
         public int DoorsQuantity { get; set; }
         public string Color { get; set; }
         public Transmission Transmission { get; set; }
+
+        //Constructors to "test" CarCRUD in Program.cs
+        public Car()
+        {
+
+        }
+
+        public Car(CarsBrands brand, string model, int doorsQuantity, string color, Transmission transmission)
+        {
+            Brand = brand;
+            Model = model;
+            DoorsQuantity = doorsQuantity;
+            Color = color;
+            Transmission = transmission;
+        }
     }
 }
