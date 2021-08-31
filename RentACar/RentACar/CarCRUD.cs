e0e9ceec12abd545c971b583855365bfb0bc8578
Patricio <pato.rocaspana@gmail.com>
2021-08-31 01:50:28 -0300

    Create empty CarCRUD class

---
+++ RentACar/RentACar/CarCRUD.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar
+{
+    public class CarCRUD
+    {
+        public Car Create(Car car)
+        {
+            // Code here
+            return new Car();
+        }
+
+        public Car Get(int id)
+        {
+            // Code here
+            return new Car();
+        }
+
+        public Car Update(Car car)
+        {
+            // Code here
+            return new Car();
+        }
+
+        public void Delete(int id)
+        {
+            // Code here
+        }
+    }
+}
