433fde1eaaca12aa66832a2e4c5a8981bc5bde71
Patricio <pato.rocaspana@gmail.com>
2021-08-31 13:59:23 -0300

    Add Create Method

--- RentACar/RentACar/CarCRUD.cs
+++ RentACar/RentACar/CarCRUD.cs
@@ -1,17 +1,42 @@
 ﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace RentACar
 {
     public class CarCRUD
     {
+        private readonly string _jsonFile = @"C:\repos\RentACar\RentACar\RentACar\DataBase\Cars.json";
+
         public Car Create(Car car)
         {
-            // Code here
-            return new Car();
+            var cars = new List<Car>();
+
+            if (File.Exists(_jsonFile))
+            {
+                var existingJsonToString = File.ReadAllText(_jsonFile);
+                cars = JsonSerializer.Deserialize<List<Car>>(existingJsonToString);
+
+                car.Id = cars.Count;
+                cars.Add(car);
+            }
+            else
+            {
+                car.Id = 0;
+                cars.Add(car);
+            }
+
+            var listToString = JsonSerializer.Serialize(cars);
+            File.WriteAllText(_jsonFile, listToString);
+
+            return car;
+
         }
 
         public Car Get(int id)
